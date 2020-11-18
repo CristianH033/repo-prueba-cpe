127891121bd64ba45fbd50081a768a18e5769c8f
juansebastiancarreno <juansebastiancarreno@live.com>
2020-11-18 11:02:34 -0500

    Django

---
+++ Ejemplo Django/example/supermarket_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SupermarketApiConfig(AppConfig):
+    name = 'supermarket_api'
---
+++ Ejemplo Django/example/supermarket_api/models/__init__.py
@@ -0,0 +1,5 @@
+#Agregando el Model Category
+from .category_model import Category 
+
+#Agregando el Model Product
+from .product_model import Product
